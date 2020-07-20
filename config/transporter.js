a7f2d35f94a5c449c0644b72bf2168c8d7fcf0a4
Yizhi Zhang <36008792+klwaamzhang@users.noreply.github.com>
2020-07-20 15:59:33 -0400

    Update transporter.js

--- config/transporter.js
+++ config/transporter.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-export const transporter = nodemailer.createTransport({
+module.exports.transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: `${process.env.GMAIL_USERNAME}`,
