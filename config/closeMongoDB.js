c4c6d3d2b0b76889a63fe97cb1052491dc1df42e
Yizhi Zhang <36008792+klwaamzhang@users.noreply.github.com>
2020-07-28 13:12:36 -0400

    Delete closeMongoDB.js

--- config/closeMongoDB.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (client) {
-  function cleanup() {
-    console.log("clean up db connection...");
-    client.close();
-  }
-
-  process.on("SIGINT", cleanup);
-  process.on("SIGTERM", cleanup);
-};
